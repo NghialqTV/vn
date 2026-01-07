25a2a5e1d04e7fc8c6a6569f315812a01fc35fd5
Nghĩalq TV <nguyentrungnghia20012004@gmail.com>
2026-01-07 20:44:30 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+const ADMIN = {
+  name: "Nghialq TV",
+  avatar: "assets/img/avatar.png",
+  telegram: "https://t.me/yourchannel"
+};
+
+const APPS = [
+  {
+    id: "aov",
+    name: "Hack Map AOV 6.0",
+    version: "Cập nhật 03/01",
+    link: "LINK_TẢI_APP"
+  }
+];
+
+const KEYS = [
+  {
+    name: "Get Key IOS",
+    info: "1.5 Day",
+    link: "LINK_GET_KEY"
+  }
+];
+
+const FILES = [
+  {
+    title: "Pack Skin Conan",
+    image: "assets/img/banner.jpg",
+    link: "LINK_TẢI_FILE"
+  }
+];
